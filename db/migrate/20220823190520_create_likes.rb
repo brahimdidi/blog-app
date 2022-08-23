a50946bc4c852c93062f3a423999abe8e1fe7c43
brahimdidi <brahimgb1team@gmail.com>
2022-08-23 21:15:18 +0000

    fix linters

--- db/migrate/20220823190520_create_likes.rb
+++ db/migrate/20220823190520_create_likes.rb
@@ -1,8 +1,9 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
+  # rubocop:disable Style/SymbolProc
   def change
     create_table :likes do |t|
-
       t.timestamps
     end
   end
+  # rubocop:enable Style/SymbolProc
 end
