9918d9f6fd5d91183a2f308b27a6ae2112adec2b
brahimdidi <brahimgb1team@gmail.com>
2022-08-24 18:23:45 +0000

    increment likes automaticly

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,8 +2,7 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  def update_likes_counter(post, likes_number)
-    post = Post.where(id: post.id)
-    post.update(likesCounter: likes_number)
+  def update_likes_counter
+    user.increment!(:likesCounter)
   end
 end
