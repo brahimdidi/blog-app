0f0cc4d4aa51955ea3493db07cf16fb8ade12a1a
brahimdidi <brahimgb1team@gmail.com>
2022-08-24 15:38:13 +0000

    fix typo (likes to posts) line 6 & 9 in post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ApplicationRecord
 
   has_many :likes
   has_many :comments
-  def update_user_posts_counter(user,likes_number)
+  def update_user_posts_counter(user,posts_number)
     user = self.where(id: user.id)
-    user.update(PostsCounter: likes_number)
+    user.update(PostsCounter: posts_number)
   end
 end
