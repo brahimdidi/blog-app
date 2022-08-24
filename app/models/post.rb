4eacfb9561cdea13d3172b10f960965d7daed2e9
brahimdidi <brahimgb1team@gmail.com>
2022-08-24 15:51:19 +0000

    fix linter

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,11 @@ class Post < ApplicationRecord
 
   has_many :likes
   has_many :comments
-  def update_user_posts_counter(user,posts_number)
+  def update_user_posts_counter(user, posts_number)
     user = User.where(id: user.id)
     user.update(PostsCounter: posts_number)
   end
+
   def last_5_comments(post)
     post = self.where(id: post.id)
     recent_comments = post.comments.limit(5).order(created_at: :desc)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
 
- def last_3_posts(user)
-  user = self.where(id: user.id)
-  recent_posts = user.posts.limit(3).order(created_at: :desc)
- end
+  def last_3_posts(user)
+    user = self.where(id: user.id)
+    recent_posts = user.posts.limit(3).order(created_at: :desc)
+    recent_posts
+  end
 end
