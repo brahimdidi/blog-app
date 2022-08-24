48596a16026ff3e5001ac8682e511b27eca8ccf5
brahimdidi <brahimgb1team@gmail.com>
2022-08-24 15:48:25 +0000

    fix typo,def meth to return recent comments for a post

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,11 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
   def update_user_posts_counter(user,posts_number)
-    user = self.where(id: user.id)
+    user = User.where(id: user.id)
     user.update(PostsCounter: posts_number)
+  end
+  def last_5_comments(post)
+    post = self.where(id: post.id)
+    recent_comments = post.comments.limit(5).order(created_at: :desc)
   end
 end
