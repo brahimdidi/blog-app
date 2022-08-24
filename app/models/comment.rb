742c9b0eeecc121dee8e6eade3d7c71bd64fedc9
brahimdidi <brahimgb1team@gmail.com>
2022-08-24 15:48:59 +0000

    def method to update commentsCounter for a post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  def update_comments_counter(post, comments_number)
+    post = Post.where(id: post.id)
+    post.update(commentsCounter: comments_number)
+  end
 end
