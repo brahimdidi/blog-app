bc99697cd937d65926b0d7af02906f489071faef
brahimdidi <brahimgb1team@gmail.com>
2022-08-26 15:36:15 +0000

    use return at the end of block to please linters

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  subject { Like.new }
-
-  before { subject.save }
   it 'should belong to user and post' do
     user = Like.reflect_on_association(:user)
     post = Like.reflect_on_association(:post)
     expect(user.macro).to eq(:belongs_to)
     expect(post.macro).to eq(:belongs_to)
   end
+  return
 end
