3ddf2e9243170862d489155b0158b001479671e7
brahimdidi <brahimgb1team@gmail.com>
2022-08-26 12:44:34 +0000

    add validation for post model, title must exitst..

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ApplicationRecord
 
   has_many :likes
   has_many :comments
+
+  validates :title, presence: true, length: { maximum: 10 }
+  validates :commentsCounter, :likesCounter, numericality: { greater_than: -1, allow_nil: true }
   def increment_posts_counter
     user.increment!(:postsCounter)
   end
