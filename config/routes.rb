a9e37407461c0ef6fd7e29bd609f5a3f780acb25
brahimdidi <brahimgb1team@gmail.com>
2022-08-29 16:56:13 +0000

    config all controlers and its view pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/users'
+  
+  resources :users,  shallow: true do
+    resources :comments
+    resources :posts
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
