b3bf48b6468619685fa43c8da85e42f1b1fd296c
brahimdidi <brahimgb1team@gmail.com>
2022-08-30 15:27:40 +0000

    test response template and body for users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,9 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /users' do
-    it 'returns http success' do
+    before :each do
       get '/users'
+    end
+    it 'returns http success' do
+      
       expect(response).to have_http_status(:success)
+    end
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
+    end
+    it 'has the correct text placeholderss' do
+      body = response.body
+      expect(body).to include('Users')
     end
   end
 end
