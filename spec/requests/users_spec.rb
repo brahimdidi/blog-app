a0eadca81d3a5d15229980678432fff5ac81c34b
brahimdidi <brahimgb1team@gmail.com>
2022-08-31 14:53:07 +0000

    add test for users routes expect http ok page render

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,16 +2,36 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
+    before :each do 
+      get '/users'
+    end
     it "returns http success" do
-      get "/users/index"
+      get "/users"
       expect(response).to have_http_status(:success)
+    end
+    it 'renders the correct template' do
+      expect(response).to render_template('index')
+    end
+    it 'has the correct text placeholderss' do
+      body = response.body
+      expect(body).to include('Users#index')
     end
   end
 
   describe "GET /show" do
+    before :each do 
+      get '/users/show'
+    end
     it "returns http success" do
-      get "/users/show"
+      get "/users/1"
       expect(response).to have_http_status(:success)
+    end
+    it 'renders the correct template' do 
+        expect(response).to render_template('show')
+    end
+    it 'has the correct text placeholderss' do
+      body = response.body
+      expect(body).to include('Users#show')
     end
   end
 
