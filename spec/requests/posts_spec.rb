d9a4d3657fabc2a8d7e494d8d057ff429a2aa65e
brahimdidi <brahimgb1team@gmail.com>
2022-08-31 14:52:49 +0000

    add test for posts routes expect http ok page render

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,16 +2,34 @@ require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
   describe "GET /index" do
+    before :each do 
+      get '/posts/index'
+    end
     it "returns http success" do
-      get "/posts/index"
       expect(response).to have_http_status(:success)
+    end
+    it 'renders the correct template' do
+      expect(response).to render_template('index')
+    end
+    it 'has the correct text placeholderss' do
+      body = response.body
+      expect(body).to include('Posts#index')
     end
   end
 
   describe "GET /show" do
+    before :each do 
+      get '/posts/show'
+    end
     it "returns http success" do
-      get "/posts/show"
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:ok)
+    end
+    it 'renders the correct template' do
+      expect(response).to render_template('show')
+    end
+    it 'has the correct text placeholderss' do
+      body = response.body
+      expect(body).to include('Posts#show')
     end
   end
 
