8f238c91f8f6620d0901aa539ed0d29e322cc555
brahimdidi <brahimgb1team@gmail.com>
2022-09-01 12:57:20 +0000

    create an instance of user, pass it to the view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,9 @@ class UsersController < ApplicationController
     @recent_posts = @user.last_3_posts
   end
   def new
-   
+    user = User.new
+    respond_to do |format|
+      format.html { render :new, locals: { user: user } }
     end
   end
 end
