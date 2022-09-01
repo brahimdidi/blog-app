77eed969928d119f30e9daea16dd16f6984d2390
brahimdidi <brahimgb1team@gmail.com>
2022-09-01 15:24:48 +0000

    fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index show] do
     resources :posts do 
-      resources: comments. only: %i[new create] 
+      resources: comments, only: %i[new create] 
     end
   end
   # Defines the root path route ("/")
