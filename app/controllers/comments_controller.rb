db8b3647ccec01e2a5e009b08e682e3db5526561
brahimdidi <brahimgb1team@gmail.com>
2022-09-01 15:28:14 +0000

    add logic to new method in commentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def new
+    @user = current_user
+    comment = Comment.new
+    respond_to do |format|
+      format.html { render :new, locals: { comment: comment } }
+   end
   end
 
   def create
