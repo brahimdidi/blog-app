f506e3192c31c0a4ac15cc629e4e9e7b80810f5d
brahimdidi <brahimgb1team@gmail.com>
2022-09-01 15:19:32 +0000

    secure_posting to db: check data with require and permit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
   end
   def create 
     @user = current_user
-    @post = @user.posts.new(title: params[:title],text: params[:text])
+    @post = @user.posts.new(params.require(:post).permit(:title, :text))
     if @post.save
       flash[:success] = "Question saved successfully"
       redirect_to user_posts_path(@user)
