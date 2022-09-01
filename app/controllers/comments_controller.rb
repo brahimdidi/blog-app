63574344e25cb62d706cd32763c93e119c0f5162
brahimdidi <brahimgb1team@gmail.com>
2022-09-01 15:47:03 +0000

    add index to comments view to show all comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,9 @@
 class CommentsController < ApplicationController
+  def index
+    @comments = Comment.where(post: @post)
+  end
   def new
+    @post = Post.find(params[:user_id])
     @user = current_user
     comment = Comment.new
     respond_to do |format|
@@ -8,6 +12,6 @@ class CommentsController < ApplicationController
   end
 
   def create
-    
+
   end
 end
