99e32e512844b139f9acee117dea4b3eb543ce55
brahimdidi <brahimgb1team@gmail.com>
2022-09-01 18:41:28 +0000

    set likes child of post with only new and create

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   resources :users, only: %i[index show] do
     resources :posts do
       resources :comments
+      resources :likes, only: %i[new create]
     end
   end
   # Defines the root path route ("/")
+   post '/likes/new', to: 'likes#create'
   # root "articles#index"
 end
