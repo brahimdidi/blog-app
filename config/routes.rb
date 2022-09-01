d4fce645317b09032d47ac02f41070845a35f791
brahimdidi <brahimgb1team@gmail.com>
2022-09-01 15:47:49 +0000

    fix typo error in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index show] do
-    resources :posts do 
-      resources: comments, only: %i[new create] 
+    resources :posts do
+      resources :comments
     end
   end
   # Defines the root path route ("/")
