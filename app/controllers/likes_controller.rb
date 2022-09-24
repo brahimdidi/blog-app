7a164dbd396ae71a45f2468141370d41059c4f1f
brahimdidi <brahimgb1team@gmail.com>
2022-09-24 16:51:37 +0000

    prevent users from liking one post many times in likescontroller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,23 @@
 class LikesController < ApplicationController
   def create
-    @like = Like.new(post_id: params[:post_id], user_id: current_user.id)
-    if @like.save
-      redirect_to user_post_path(@like.post.user, @like.post)
-      @like.update_likes_counter
-      flash[:notice] = 'You just liked this post'
+    
+
+
+
+    if !Like.all.where(user_id: current_user.id, post_id: params[:post_id]).empty?
+      @post = Post.find(params[:post_id])
+      redirect_to request.path
+      return flash[:alert] = 'You already liked this post'
     else
-      flash.now[:notice] = 'Error:Like could not be saved'
+      @like = Like.new(post_id: params[:post_id], user_id: current_user.id) 
+      if @like.save
+        redirect_to user_post_path(@like.post.user, @like.post)
+        @like.update_likes_counter
+        flash[:notice] = 'You just liked this post'
+      else
+        flash.now[:notice] = 'Error:Like could not be saved'
+      end
+
     end
   end
 end
