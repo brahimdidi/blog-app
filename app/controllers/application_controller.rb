c7ad0d21da88f4475b377dbe94b6b80b56b313c1
brahimdidi <brahimgb1team@gmail.com>
2022-09-27 07:31:46 +0000

    require authentication to use app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user! #-> routes to the login / signup if not authenticated
+
+
   protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
