d308e8b6c2d5cb59f2c72baa002df9a6abddc021
brahimdidi <brahimgb1team@gmail.com>
2022-10-11 14:16:04 +0000

    generate random posts in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+1000.times do 
+  @text = (0...189).map { ('a'..'z').to_a[rand(26)] }.join
+  @user_id  = rand(1..3)
+  @post_id = rand(1..10100)
+  Post.create.create(text: @text, user_id: @user_id )
+end
