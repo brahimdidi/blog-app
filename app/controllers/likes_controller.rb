64d9ef9999e4b999880e9d2c4429815802f32180
brahimdidi <brahimgb1team@gmail.com>
2023-01-13 20:51:51 +0000

    add unlike post option in likes controller and model

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,19 +1,20 @@
 class LikesController < ApplicationController
   def create
     if !Like.all.where(user_id: current_user.id, post_id: params[:post_id]).empty?
-      @post = Post.find(params[:post_id])
+      like = Like.all.where(user_id: current_user.id, post_id: params[:post_id]).first;
+      like.destroy
+      like.decrement_likes_counter
       redirect_to request.path
-      flash[:alert] = 'You already liked this post'
+      flash[:alert] = 'post was unliked'
     else
       @like = Like.new(post_id: params[:post_id], user_id: current_user.id) 
       if @like.save
-        redirect_to user_post_path(@like.post.user, @like.post)
-        @like.update_likes_counter
+        redirect_to request.path
+        @like.increment_likes_counter
         flash[:notice] = 'You just liked this post'
       else
         flash.now[:notice] = 'Error:Like could not be saved'
       end
-
     end
   end
 end
