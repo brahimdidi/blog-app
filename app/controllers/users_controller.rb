33660f0051f2f12bdb4c3edbbe5e7b32d87758f6
brahimdidi <brahimgb1team@gmail.com>
2023-01-14 20:35:29 +0000

    use friendly id to show user in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
   def index
     unless current_user.nil?
-      @users = User.all.where.not(id: current_user.id)
+      @users = User.all.where.not(id: current_user.id).includes(:posts);
     end
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.friendly.find(params[:id])
     @recent_posts = @user.last_3_posts
   end
 end
